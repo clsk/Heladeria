76c7425ea70ef0d34d1cb40b4e873b9b5e80c51f
Alan Alvarez <aalvarez@aliensoft.net>
2013-10-24 16:55:42 -0400

    Initial import

---
+++ DataLayer/BasePersona.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public abstract class BasePersona : IPersona
+    {
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad nombre.
+        /// </summary>
+        private string _nombre;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Nombre.
+        /// </summary>
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad apellido.
+        /// </summary>
+        private string _apellido;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Apellido.
+        /// </summary>
+        public string Apellido
+        {
+            get
+            {
+                return _apellido;
+            }
+            set
+            {
+                _apellido = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Nombre.
+        /// </summary>
+        private string _identificacion;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad NoIdentificacion.
+        /// </summary>
+        public string NoIdentificacion
+        {
+            get
+            {
+                return _identificacion;
+            }
+            set
+            {
+                _identificacion = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad FechaImportante.
+        /// </summary>
+        private string _fecha;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Fecha Importante.
+        /// </summary>
+        public string FechaImportante
+        {
+            get
+            {
+                return _fecha;
+            }
+            set
+            {
+                _fecha = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Telefono.
+        /// </summary>
+        private string _telefono;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Telefono.
+        /// </summary>
+        public string Telefono
+        {
+            get
+            {
+                return _telefono;
+            }
+            set
+            {
+                _telefono = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Calle.
+        /// </summary>
+        private string _calle;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Calle.
+        /// </summary>
+        public string Calle
+        {
+            get
+            {
+                return _calle;
+            }
+            set
+            {
+                _calle = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Sector.
+        /// </summary>
+        private string _sector;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Sector.
+        /// </summary>
+        public string Sector
+        {
+            get
+            {
+                return _sector;
+            }
+            set
+            {
+                _sector = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Çiudad.
+        /// </summary>
+        private string _ciudad;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Ciudad.
+        /// </summary>
+        public string Ciudad
+        {
+            get
+            {
+                return _ciudad;
+            }
+            set
+            {
+                _ciudad = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Provincia.
+        /// </summary>
+        private string _provincia;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad Provincia.
+        /// </summary>
+        public string Provincia
+        {
+            get
+            {
+                return _provincia;
+            }
+            set
+            {
+                _provincia = value;
+            }
+        }
+
+        /// <summary>
+        /// Atributo privado que define dentro de la clase
+        /// la propiedad Correo Electrónico.
+        /// </summary>
+        private string _correo;
+
+        /// <summary>
+        /// Implementacion correspondiente a los metodos
+        /// de la propiedad CorreoElectronico.
+        /// </summary>
+        public string CorreoElectronico
+        {
+            get
+            {
+                return _correo;
+            }
+            set
+            {
+                _correo = value;
+            }
+        }
+    }
+}
---
+++ DataLayer/IPersona.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+        public interface IPersona
+        {
+            /// <summary>
+            /// Propiedad que define el Nombre de una Persona
+            /// </summary>
+            string Nombre { get; set; }
+
+            /// <summary>
+            /// Propiedad que define el Apellido de una Persona
+            /// </summary>
+            string Apellido { get; set; }
+
+            /// <summary>
+            /// Propiedad que define el Numero de Identificación de una Persona.
+            /// </summary>
+            /// <remarks>
+            /// Esta propiedad adimite el empleo de RNC y Cedula.
+            /// </remarks>
+            string NoIdentificacion { get; set; }
+
+            /// <summary>
+            /// Propiedad para deifinir Fecha Importante Respecto a una Persona.
+            /// </summary>
+            /// <remarks>
+            /// Esta propiedad admite el empleo de Fecha de Ingreso y Fecha de Nacimiento.
+            /// </remarks>
+            string FechaImportante { get; set; }
+
+            /// <summary>
+            /// Propiedad que define el Numero de Telefono de una Persona
+            /// </summary>
+            string Telefono { get; set; }
+
+            /// <summary>
+            /// Propiedad que define la Calle en la que Reside una Persona
+            /// </summary>
+            string Calle { get; set; }
+
+            /// <summary>
+            /// Propiedad que define el Sector en el que Reside una Persona.
+            /// </summary>
+            string Sector { get; set; }
+
+            /// <summary>
+            /// Propiedad que define la Ciudad en que vive una Persona.
+            /// </summary>
+            string Ciudad { get; set; }
+
+            /// <summary>
+            /// Propiedad que define la Provincia en que vive una Persona.
+            /// </summary>
+            string Provincia { get; set; }
+
+            /// <summary>
+            /// Propiedad que define el Correo Electronico de una Persona.
+            /// </summary>
+            /// <remarks>
+            /// Es importante tener otras formas de contactar con una Persona.
+            /// </remarks>
+            string CorreoElectronico { get; set; }
+        }
+}
